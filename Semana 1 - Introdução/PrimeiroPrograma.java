4edc910e8a07a3bf7d69eaf91f6692d4f2cf6087
AnaDuarte1 <anaberocha@hotmail.com>
2024-09-25 22:09:55 -0300

    Primeiro Programa Java

---
+++ Semana 1 - Introdução/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+//Primeiro programa em Java
+
+public class PrimeiroPrograma{ //Classe com o mesmo nome do arquivo
+    public static void main(String[] args){ //metodo principal, tipo o "main" 
+        System.out.println("Primeiro programa java!"); //saída padrão
+        System.out.println("Olá mundo!");
+    }
+}
+
+//Terminal do Visual:
+
+// Entra na pasta Semana 1 - Introdução com CD
+// LS para mostrar os arquivos da pasta
+// Javac PrimeiroPrograma.java - Compila o código
+// Java PrimeiroPrograma.java - Executa o código
+
+//Terminal do computador:
+
+// Entra na pasta htdocs com CD
+// Entra na pasta INOO com CD
+// Entra na pasta Semana 1 - Introdução com CD
+// LS para mostrar os arquivos da pasta
+// Javac PrimeiroPrograma.java - Compila o código
+// Java PrimeiroPrograma.java - Executa o código
