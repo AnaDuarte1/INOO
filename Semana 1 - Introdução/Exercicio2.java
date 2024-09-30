d5935c0d714dd5f53be20f29e79f8ba0b4152e8a
Ana Duarte <anaberocha@hotmail.com>
2024-09-30 13:53:12 -0300

    exercicios iniciais

---
+++ Semana 1 - Introdução/Exercicio2.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Exercicio2{
+    
+	public static void main(String[] args) {
+		int tempF = 200;
+		int C;
+		
+		C = (tempF - 32);
+		System.out.println("Temperatura Fahrenheit: " + tempF + " Temperatura Celsius: " + C );
+	}
+}
