dfb64a059fc0c12553b6c66a124b5b7118a66930
Ana Beatriz Rocha Duarte <83419660+AnaDuarte1@users.noreply.github.com>
2024-10-09 21:39:15 -0300

    Add files via upload

---
+++ ex1/src/com/Ana/academico/model/Aluno.java
@@ -0,0 +1,14 @@
+package com.Ana.academico.model;
+
+public class Aluno {
+    public String nome;
+    private int idade;
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade; //Atributo da Classe 
+    }
+}
